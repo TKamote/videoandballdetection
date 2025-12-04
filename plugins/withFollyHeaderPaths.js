1ebc464809c41e418b2ddc4dd499571bc25c69a8
KamoteNga <kamoteka143din@gmail.com>
2025-12-04 22:08:57 +0800

    fix: Correct faulty logic in Folly config plugin

--- plugins/withFollyHeaderPaths.js
+++ plugins/withFollyHeaderPaths.js
@@ -2,6 +2,20 @@ const { withDangerousMod } = require('@expo/config-plugins');
 const fs = require('fs');
 const path = require('path');
 
+// This is the Ruby code we want to inject into the post_install block
+const codeToInject = `
+  installer.pods_project.targets.each do |target|
+    if target.name == 'React-cxxreact'
+      target.build_configurations.each do |config|
+        config.build_settings['HEADER_SEARCH_PATHS'] ||= ['$(inherited)']
+        config.build_settings['HEADER_SEARCH_PATHS'] << '"$(PODS_ROOT)/boost"'
+        config.build_settings['HEADER_SEARCH_PATHS'] << '"$(PODS_ROOT)/glog"'
+        config.build_settings['HEADER_SEARCH_PATHS'] << '"$(PODS_ROOT)/Folly"'
+      end
+    end
+  end
+`;
+
 const withFollyHeaderPaths = (config) => {
   return withDangerousMod(config, [
     'ios',
@@ -9,31 +23,23 @@ const withFollyHeaderPaths = (config) => {
       const podfilePath = path.join(config.modRequest.projectRoot, 'ios', 'Podfile');
       let podfileContent = fs.readFileSync(podfilePath, 'utf8');
 
-      // Add the post_install hook
-      const postInstallHook = `
-post_install do |installer|
-  installer.pods_project.targets.each do |target|
-    if target.name == 'React-cxxreact'
-      target.build_configurations.each do |config|
-        config.build_settings['HEADER_SEARCH_PATHS'] ||= '$(inherited)'
-        config.build_settings['HEADER_SEARCH_PATHS'] << '"$(PODS_ROOT)/boost"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << '"$(PODS_ROOT)/glog"'
-        config.build_settings['HEADER_SEARCH_PATHS'] << '"$(PODS_ROOT)/Folly"'
-      end
-    end
-  end
-end
-`;
+      // A unique string to check if we've already injected the code
+      const checkString = "config.build_settings['HEADER_SEARCH_PATHS'] << '\"$(PODS_ROOT)/Folly\"'";
 
-      // Ensure the hook is not already added
-      if (!podfileContent.includes("config.build_settings['HEADER_SEARCH_PATHS'] << '\"$(PODS_ROOT)/Folly\"'")) {
+      if (podfileContent.includes(checkString)) {
+        // Already patched, no changes needed
+        return config;
+      }
+
+      if (podfileContent.includes('post_install do |installer|')) {
+        // If the block exists, inject our code after the first line.
         podfileContent = podfileContent.replace(
-          /post_install do \|installer\|/,
-          `post_install do |installer|${postInstallHook.replace('post_install do |installer|', '')}`
+          'post_install do |installer|',
+          `post_install do |installer|${codeToInject}`
         );
-         if (!podfileContent.includes('post_install do |installer|')) {
-            podfileContent += postInstallHook;
-         }
+      } else {
+        // If the block doesn't exist for some reason, add the whole thing.
+        podfileContent += `\npost_install do |installer|${codeToInject}\nend\n`;
       }
 
       fs.writeFileSync(podfilePath, podfileContent);
